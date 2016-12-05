3a300232b1ff9476e1956309bdc5b47db6ee89c6
Evan Feng <evanfeng55@gmail.com>
2016-12-05 14:36:31 -0500

    fixed read null case for test5

--- src/com/yf833/DBSite.java
+++ src/com/yf833/DBSite.java
@@ -7,113 +7,124 @@ import java.util.HashSet;
 // each site represents a separate database -- it has its own lock table and variable copies
 public class DBSite {
 
-  // the id for this site
-  public int id;
+    // the id for this site
+    public int id;
 
-  // track if the site is up or not
-  public boolean isFailed;
+    // track if the site is up or not
+    public boolean isFailed;
 
-  // the variables contained at this site
-  public HashSet<Integer> variables;
+    // the variables contained at this site
+    public HashSet<Integer> variables;
 
-  // pending write requests for this site (transaction_id --> [W(x1,val), W(x2,val), ...])
-  public HashMap<Integer, ArrayList<Action>> pendingactions;
+    // pending write requests for this site (transaction_id --> [W(x1,val), W(x2,val), ...])
+    public HashMap<Integer, ArrayList<Action>> pendingactions;
 
-  // the actual data values stored at this site (variable --> value)
-  public HashMap<Integer, Integer> datatable;
+    // the actual data values stored at this site (variable --> value)
+    public HashMap<Integer, Integer> datatable;
 
-  // the locktable for this site (variable --> {type: W, transac_id: i})
-  public HashMap<Integer, ArrayList<LockEntry>> locktable;
+    // the locktable for this site (variable --> {type: W, transac_id: i})
+    public HashMap<Integer, ArrayList<LockEntry>> locktable;
 
-  boolean hasRecovered;
 
-  public DBSite(int id) {
-    this.id = id;
+    boolean hasRecovered;
 
-    this.locktable = new HashMap<>();
-    this.variables = new HashSet<>();
-    this.isFailed = false;
-    pendingactions = new HashMap<>();
+    public DBSite(int id) {
+        this.id = id;
 
-    // var_id --> value
-    this.datatable = new HashMap<>();
-    this.hasRecovered = false;
+        this.locktable = new HashMap<>();
+        this.variables = new HashSet<>();
+        this.isFailed = false;
+        pendingactions = new HashMap<>();
 
-  }
+        // var_id --> value
+        this.datatable = new HashMap<>();
+        this.hasRecovered = false;
 
-  // commit the specified transaction
-  public void commit(int transac_id, Transaction.Type transac_type) throws Exception {
 
-    removeAllLocksForTransaction(transac_id);
-
-    // if this site doesn't have any pending actions for this transaction, then exit (nothing to
-    // commit)
-    if (!this.pendingactions.keySet().contains(transac_id)) {
-      return;
     }
 
-    // execute all pending actions for this transaction
-    for (Action a : this.pendingactions.get(transac_id)) {
+    // commit the specified transaction
+    public void commit(int transac_id, Transaction.Type transac_type) throws Exception {
 
-      if (a.type.equalsIgnoreCase("W")) {
-        this.datatable.put(a.variable, a.value);
-      } else if (a.type.equalsIgnoreCase("R") && transac_type == Transaction.Type.DEFAULT) {
-        System.out.println("\nREAD: T" + a.transac_id + " reads x" + a.variable + "="
-            + this.datatable.get(a.variable));
-      } else if (a.type.equalsIgnoreCase("R") && transac_type == Transaction.Type.READONLY) {
-        // do nothing
-      } else {
-        System.out.println("ERROR: Invalid action type in the pendingactions queue");
-        throw new Exception();
-      }
-    }
+        removeAllLocksForTransaction(transac_id);
 
-    // remove the row for this transaction in pending actions
-    this.pendingactions.remove(transac_id);
-
-  }
-
-  // abort the specified transaction
-  public void abort(int transac_id) {
-    // clear the transaction's pending writes at this site
-
-    // if site s contains pending for this transaction, remove that transaction's row from the
-    // pending writes table
-    if (this.pendingactions.keySet().contains(transac_id)) {
-      this.pendingactions.remove(transac_id);
-    }
-
-    removeAllLocksForTransaction(transac_id);
-
-  }
-
-  // free all locks for this transaction
-  private void removeAllLocksForTransaction(int transac_id) {
-    for (int var_id : this.locktable.keySet()) {
-      ArrayList<LockEntry> new_lelist = new ArrayList<>();
-      // find the indexes to remove
-      for (LockEntry le : this.locktable.get(var_id)) {
-        // if this lockentry belongs to the committed transaction, then remove it
-        if (le.transac_id != transac_id) {
-          new_lelist.add(le);
+        //if this site doesn't have any pending actions for this transaction, then exit (nothing to commit)
+        if(!this.pendingactions.keySet().contains(transac_id)){
+            return;
         }
-      }
-      this.locktable.put(var_id, new_lelist);
-    }
-  }
 
-  // clear the lock table (free all locks for all transactions)
-  public void clearLockTable() {
-    for (int var_id : this.locktable.keySet()) {
-      this.locktable.get(var_id).clear();
-    }
-  }
+        //execute all pending actions for this transaction
+        for(Action a : this.pendingactions.get(transac_id)){
 
-  public void failure() {
-    isFailed = true;
-    pendingactions.clear();
-    locktable.clear();
-    datatable.clear();
-  }
+            if(a.type.equalsIgnoreCase("W")){
+                this.datatable.put(a.variable, a.value);
+            }
+            else if(a.type.equalsIgnoreCase("R") && transac_type == Transaction.Type.DEFAULT && this.datatable.keySet().contains(a.variable)){
+                System.out.println("\nREAD: T" + a.transac_id + " reads x" + a.variable + "=" + this.datatable.get(a.variable));
+            }
+            else if(a.type.equalsIgnoreCase("R") && transac_type == Transaction.Type.DEFAULT && !this.datatable.keySet().contains(a.variable)){
+                //do nothing if this site doesn't contain the variable that is meant to be read
+            }
+            else if(a.type.equalsIgnoreCase("R") && transac_type == Transaction.Type.READONLY){
+                //do nothing
+            }
+            else{
+                System.out.println("ERROR: Invalid action type in the pendingactions queue");
+                throw new Exception();
+            }
+        }
+
+        //remove the row for this transaction in pending actions
+        this.pendingactions.remove(transac_id);
+
+    }
+
+
+    // abort the specified transaction
+    public void abort(int transac_id) {
+        // clear the transaction's pending writes at this site
+
+        //if site s contains pending for this transaction, remove that transaction's row from the pending writes table
+        if(this.pendingactions.keySet().contains(transac_id)){
+            this.pendingactions.remove(transac_id);
+        }
+
+        removeAllLocksForTransaction(transac_id);
+
+    }
+
+
+    // free all locks for this transaction
+    private void removeAllLocksForTransaction(int transac_id){
+        for(int var_id : this.locktable.keySet()) {
+            ArrayList<LockEntry> new_lelist = new ArrayList<>();
+            //find the indexes to remove
+            for (LockEntry le : this.locktable.get(var_id)) {
+                //if this lockentry belongs to the committed transaction, then remove it
+                if (le.transac_id != transac_id) {
+                    new_lelist.add(le);
+                }
+            }
+            this.locktable.put(var_id, new_lelist);
+        }
+    }
+
+
+    //clear the lock table (free all locks for all transactions)
+    public void clearLockTable(){
+        for(int var_id : this.locktable.keySet()){
+            this.locktable.get(var_id).clear();
+        }
+    }
+
+
+
+
+    public void failure() {
+        isFailed = true;
+        pendingactions.clear();
+        locktable.clear();
+        datatable.clear();
+    }
 
 }
